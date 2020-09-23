0f7086e57fe8cce35a7481afb7837b0121c27865
Katie Jurgenson <katiejurgenson@users.noreply.github.com>
2020-09-23 15:48:45 -0500

    while form
    Did the first while form code

---
+++ forms/while/while.js
@@ -0,0 +1,9 @@
+let userChoice = prompt("How many times do you want the loop to run?")
+let i = 1 
+
+  while(i <= userChoice) {
+    console.log(`This is loop iteration ${i}.`);
+    i++;
+  }
+    
+console.log("The loop is now done."); 
